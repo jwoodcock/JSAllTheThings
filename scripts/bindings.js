9dfe5f9526d414dbc607c13d61c9f4f98b1e4bc9
Jacques Woodcock <jacques@kitewebconsulting.com>
2014-01-26 21:03:06 -0600

    Found a fault of the original binding model which would not allow it to handle multiple user entries.

--- scripts/bindings.js
+++ scripts/bindings.js
@@ -4,6 +4,7 @@
  * 
  * @param {String} objectId Id of the object to bind to
  */
+// explain how this class is different that our prior classes
 function dataBinder( objectId ) {
 
     // Create the actual publish and subscription object
@@ -37,16 +38,40 @@ function dataBinder( objectId ) {
     }
 
     // setup a couple properties we'll need
-    data_attr = "data-bind-" + object_id,
-    message = object_id + ":change",
+    // when instantiating multiple objets these properties don't work
+    data_attr = "data-bind-" + objectId,
+    message = objectId + ":change",
 
     // define a proxy method to be called on actual change
     changeHandler = function( evt ) {
         var target = evt.target,
-        prop_name = target.getAttribute( data_attr );
+        dataAttr = getAttrFromTarget( evt.target.id ),
+        prop_name = target.getAttribute( data_attr ); // should be able to remove
 
-        if ( prop_name && prop_name !== "" ) {
-            pubSub.trigger( message, prop_name, target.value );
+        data_attr = dataAttr[0];
+        message = dataAttr[1] + ':change';
+
+        if ( dataAttr && dataAttr !== "" ) {
+            pubSub.trigger( dataAttr[1] + ':change', dataAttr[0], target.value );
+        }
+    };
+
+    // get the target data-bind attribute
+    getAttrFromTarget = function( tar ) {
+        var attributes = document.getElementById( tar ).attributes;
+        for ( var p = 0; p < attributes.length; p++ ) {
+            // current attribute
+            var curAttr = attributes[ p ].name;
+            // build a substring to do check against
+            if ( typeof curAttr === 'string' ) {
+                var subStr = curAttr.substring( 0, 9 );
+                var user = curAttr.substring(10);
+                if ( subStr === 'data-bind' ) {
+                    console.log('subStr', subStr);
+                    console.log('subStr', user);
+                    return [ attributes[ p ].value, user ];
+                }
+            }
         }
     };
 
