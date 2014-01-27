4a47e31bd68534e272765ca6ecc23c4c44f60181
Jacques Woodcock <jacques@kitewebconsulting.com>
2014-01-26 21:02:12 -0600

    Built out a simple ajax object to query the html files for the templates.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,1 +1,14 @@
 // ajax.js is here
+function ajax() {
+
+    // build request object
+    var request = new XMLHttpRequest();
+    var requestType = 'GET';
+
+    // create make a request method
+    this.makeRequest = function( path ) {
+        request.open( requestType, path, false);
+        request.send(null);
+        return request.responseText;
+    }
+}
